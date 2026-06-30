24f2eeb506715b8c7378c15c2485610594c8de22
zangjinkai-11 <zangjinkai1@gmail.com>
2026-06-30 09:50:50 +0800

    增加了一些异常处理

--- EEG Explorer/reader.py
+++ EEG Explorer/reader.py
@@ -9,6 +9,7 @@ Returns:
     pandas.DataFrame
 """
 # 开始->检查文件是否存在->try读取->except处理错误->读取成功->打印：shape,columns->返回DataFrame
+
 def read_csv(path: str):
     try:
         df=pd.read_csv(path,encoding='utf-8')
