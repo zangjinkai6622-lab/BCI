1431d9428cd152686b380d46054e8981ef2e10d5
zangjinkai-11 <zangjinkai1@gmail.com>
2026-07-02 09:56:09 +0800

    EEG Explorer/analyser.py，新增了分析DataFrame特征的方法

---
+++ EEG Explorer/analyser.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def get_shape(df: pd.DataFrame):
+    return df.shape
+def get_columns(df: pd.DataFrame):
+    return list(df.columns)
+def get_missing_values(df: pd.DataFrame):
+    return df.isnull().sum()
+def get_data_type(df: pd.DataFrame):
+    return df.dtypes
+    
+def get_statistics(df:pd.DataFrame):
+    return df.describe()
--- EEG Explorer/main.py
+++ EEG Explorer/main.py
@@ -1,16 +1,25 @@
 from reader import read_csv
+from analyser import *
 
 
-
-def main():
-    path='https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv'
-    df=read_csv('https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv')
+def main(path: str):
+    df=read_csv(path)
     if df is None:
         return
-    print(df.shape)
-    print(df.columns)
-    print("读取成功")
+    # print(df.shape)
+    # print(df.columns)
+    # print("读取成功")
+    rows,columns=get_shape(df)
+    print("rows:",rows)
+    print("columns:",columns)
+    print(get_columns(df))
+    print(get_missing_values(df))
+    print(get_data_type(df))
+    print(get_statistics(df))
+
+
 if __name__ == '__main__':
-    main()
+    path='https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv'
+    main(path)
 
 
