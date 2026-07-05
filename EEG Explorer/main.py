3472a8e58a4789ee4347eabd552f4a908b887120
zangjinkai-11 <zangjinkai1@gmail.com>
2026-07-05 11:53:40 +0800

    o优化业务逻辑，result更规范

--- EEG Explorer/main.py
+++ EEG Explorer/main.py
@@ -1,29 +1,27 @@
-from reader import read_csv
+import reader  
 import analyser 
 import visualization 
 import report
 
-
 def main(path: str):
-    df=read_csv(path)
+    df=reader.read_csv(path)
     if df is None:
         return
-    statistics=analyser.get_statistics(df)
-    missing_values=analyser.get_missing_values(df)
-    data_type=analyser.get_data_type(df)
-    age_line1= visualization.plot_line(df,'Age','age_line1.png')
-    age_hist1= visualization.plot_histogram(df,'Age','age_hist1.png')
-    figures=[age_line1,age_hist1]
     analysis_result={
-        'statistics':statistics,
-        'missing_values':missing_values,
-        'data_type':data_type,
-        'figures':figures
+        'dataset': {
+            'rows':analyser.get_shape(df)[0],
+            'columns':analyser.get_shape(df)[1]
+
+        },
+        'statistics':analyser.get_statistics(df),
+        'missing_values':analyser.get_missing_values(df),
+        'data_type':analyser.get_data_type(df),
+        'figures':[visualization.plot_line(df,'Age','age_line1.png'),
+                  visualization.plot_histogram(df,'Age','age_hist1.png')
+                  ]
     }
-    report.generate_report(analysis_result)
+    report.generate_report(analysis_result,'report.md')
     
-
-
 if __name__ == '__main__':
     path='https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv'
     main(path)
