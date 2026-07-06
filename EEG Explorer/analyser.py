86fef29bea68321eb864f8e58cbcb70803013e21
zangjinkai-11 <zangjinkai1@gmail.com>
2026-07-06 08:01:13 +0800

    优化dataset

--- EEG Explorer/analyser.py
+++ EEG Explorer/analyser.py
@@ -1,9 +1,20 @@
 import pandas as pd
 
-def get_shape(df: pd.DataFrame):
-    return df.shape
-def get_columns(df: pd.DataFrame):
-    return list(df.columns)
+
+def get_dataset_info(df: pd.DataFrame):
+    return {
+        'rows':df.shape[0],
+        'columns':df.shape[1],
+        "column_names": list(df.columns), 
+        "memory_usage": df.memory_usage(deep=True).sum()/1024**2,
+    }
+# def get_shape(df: pd.DataFrame):
+#     return {
+#         'rows':df.shape[0],
+#         'columns':df.shape[1]
+#     }
+# def get_columns(df: pd.DataFrame):
+#     return list(df.columns)
 def get_missing_values(df: pd.DataFrame):
     return df.isnull().sum()
 def get_data_type(df: pd.DataFrame):
