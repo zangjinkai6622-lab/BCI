d6c54e23f6544c88a06eee333b3b1678ff52bde1
zangjinkai-11 <zangjinkai1@gmail.com>
2026-07-06 08:05:46 +0800

    网格画图，设置size，    plt.tight_layout()

--- EEG Explorer/visualization.py
+++ EEG Explorer/visualization.py
@@ -3,11 +3,13 @@ import config
 import pandas as pd
 
 def plot_line(df:pd.DataFrame,column:str,filename:str):
-    plt.figure()
+    plt.figure(figsize=(10,4))
     plt.title(column)
     plt.xlabel('index')
     plt.ylabel(column)
     plt.plot(df[column])
+    plt.grid(True)
+    plt.tight_layout()
     plt.savefig(config.FIGURE_DIR/filename)
     plt.close()
     return filename
@@ -17,6 +19,8 @@ def plot_histogram(df:pd.DataFrame,column:str,filename:str):
     plt.xlabel(column)
     plt.ylabel('frequency')
     plt.hist(df[column])
+    plt.grid(True)
+    plt.tight_layout()
     plt.savefig(config.FIGURE_DIR/filename)
     plt.close()
     return filename
