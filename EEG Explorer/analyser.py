7ed8f4a2db97d70a2cc474a2401fe07eecd9b9df
zangjinkai-11 <zangjinkai1@gmail.com>
2026-07-07 09:24:45 +0800

    新增了time_domain_feature，分析时域特征的各个特征，重点是找到数值列，for循环返回完整字典

--- EEG Explorer/analyser.py
+++ EEG Explorer/analyser.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 
 def get_dataset_info(df: pd.DataFrame):
     return {
@@ -8,13 +8,6 @@ def get_dataset_info(df: pd.DataFrame):
         "column_names": list(df.columns), 
         "memory_usage": df.memory_usage(deep=True).sum()/1024**2,
     }
-# def get_shape(df: pd.DataFrame):
-#     return {
-#         'rows':df.shape[0],
-#         'columns':df.shape[1]
-#     }
-# def get_columns(df: pd.DataFrame):
-#     return list(df.columns)
 def get_missing_values(df: pd.DataFrame):
     return df.isnull().sum()
 def get_data_type(df: pd.DataFrame):
@@ -22,5 +15,19 @@ def get_data_type(df: pd.DataFrame):
     
 def get_statistics(df:pd.DataFrame):
     return df.describe()
-# def get_eeg_feature(df:pd.DataFrame):
-#     return df.iloc[:,1:]
+def get_time_domain_features(df:pd.DataFrame):
+    numerical_columns = df.select_dtypes(include=['float64', 'int64']).columns
+    features={}
+    for column in numerical_columns:
+        channel_feature={
+            "mean": df[column].mean(),
+            "variance": df[column].var(),
+            "std": df[column].std(),
+            "rms": np.sqrt(np.mean(df[column]**2)),
+            "peak_to_peak": np.ptp(df[column]),
+            "zero_crossing_rate": ...
+        }
+        features[column]=channel_feature
+
+    return features
+        
