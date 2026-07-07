b5eee75c3b657f1867602aae76ff89aae8a3b1e1
zangjinkai-11 <zangjinkai1@gmail.com>
2026-07-07 09:26:21 +0800

    将feature写成df形式

--- EEG Explorer/report.py
+++ EEG Explorer/report.py
@@ -1,5 +1,6 @@
 import config
 import pathlib
+import pandas as pd
 def generate_report(analysis_result:dict,filename:str):
 
     with open(config.REPORT_DIR/filename,"w",encoding="utf-8") as file:
@@ -11,12 +12,17 @@ def generate_report(analysis_result:dict,filename:str):
         file.write(analysis_result['statistics'].to_markdown()+"\n")
         file.write(f"## 3. Missing Values\n")
         file.write(f"{analysis_result['missing_values'].to_markdown()}\n")
-        file.write(f"## 4. Data Types\n")
-        file.write(f"{analysis_result['data_type'].to_markdown()}\n")
-        file.write(f"## 5. Figures\n")
+        file.write(f"## 4. Data Types\n")               
+        file.write(f"{analysis_result['data_type'].to_markdown()}\n") 
+        file.write(f"## 5. Time Domain Features\n")
+        time_feature_df=pd.DataFrame(analysis_result['time_features'])
+        file.write(f"{time_feature_df.to_markdown()}")
+        file.write("\n\n")
+
+        file.write(f"## 6. Figures\n")
         for name in analysis_result['figures']:
             file.write(f"- ![](../figures/{name})\n")
         
-        file.write(f"## 6. Interpretatio\n")
+        file.write(f"## 7. Interpretatio\n")
         file.write(f"{analysis_result['interpretation']}\n")
-        file.write(f"## 7. Conclusion\n")
+        file.write(f"## 8. Conclusion\n")
