d6aedbf4752d01fba611b6f7fa761324455a50e5
zangjinkai-11 <zangjinkai1@gmail.com>
2026-07-08 11:29:08 +0800

    加ZCR的算法

--- EEG Explorer/analyser.py
+++ EEG Explorer/analyser.py
@@ -15,17 +15,51 @@ def get_data_type(df: pd.DataFrame):
     
 def get_statistics(df:pd.DataFrame):
     return df.describe()
+
+def get_mean(df:pd.DataFrame,column:str):
+    return df[column].mean()
+
+def get_variance(df:pd.DataFrame,column:str):
+    return df[column].var()
+
+def get_standard_deviation(df:pd.DataFrame,column:str):
+    return df[column].std()
+
+def get_rms(df:pd.DataFrame,column:str):
+    return np.sqrt(np.mean(df[column]**2))
+
+def get_ptp(df:pd.DataFrame,column:str):
+    return np.ptp(df[column])
+
+def get_zero_crossing_rate(df:pd.DataFrame,column:str):
+    signal=df[column].to_numpy()
+    sign=np.sign(signal)
+    count=0
+    previous_sign = None
+    for i in range(len(sign)):
+        if sign[i] != 0:
+            if previous_sign is not None:
+                if sign[i] != previous_sign:
+                    count += 1
+                    
+            previous_sign = sign[i]
+
+        if sign[i] == 0:
+            continue
+    return count
+                   
+
 def get_time_domain_features(df:pd.DataFrame):
     numerical_columns = df.select_dtypes(include=['float64', 'int64']).columns
     features={}
     for column in numerical_columns:
         channel_feature={
-            "mean": df[column].mean(),
-            "variance": df[column].var(),
-            "std": df[column].std(),
-            "rms": np.sqrt(np.mean(df[column]**2)),
-            "peak_to_peak": np.ptp(df[column]),
-            "zero_crossing_rate": ...
+            "mean": get_mean(df,column),
+            "variance": get_variance(df,column),
+            "std": get_standard_deviation(df,column),
+            "rms": get_rms(df,column),
+            "peak_to_peak": get_ptp(df,column),
+            "zero_crossing_rate": get_zero_crossing_rate(df,column)           
         }
         features[column]=channel_feature
 
