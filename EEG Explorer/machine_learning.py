b2cb08effc930200099f73447357f3424e83bf98
zangjinkai-11 <zangjinkai6622@gmail.com>
2026-07-20 10:00:20 +0800

    sklearn完整流程

--- EEG Explorer/machine_learning.py
+++ EEG Explorer/machine_learning.py
@@ -1,13 +1,43 @@
 import  pandas as pd
-import config
-def split_windows(df, window_size):
-    windows=[]
-    for start in range(0,len(df),window_size):
-        end=start+window_size
-        window=df.iloc[start:end]
-        windows.append(window)
-    return windows
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 
+# 流程： 分离dataset的X，y ---> 分别对X,y分出train、test，随机种子42，分层提取，---> 标准化 ---> 训练模型 ---> 预测，根据X_train预测y_pred ---> 评价，根据y_pred和y_test进行准确性和混淆矩阵的评价
+def split_xy(dataset:pd.DataFrame):
+    # 测试数据集
+    X=dataset.drop('label',axis=1)
+    # 标签数据集
+    y=dataset['label']
+    return X,y
 
+def split_dataset(X:pd.DataFrame,y:pd.DataFrame):
+    # X,y原始的数据集，test_size测试的比例，random_state随机数种子，让随机操作的结果每次运行都一样。stratify（分层）标签数据集，在取测试用例时分层取样
+    # stratify传入一个数组，按照这个数组进行分层，也就是说同label分层，提取测试用例时，每层所占比例相同，都为test_size
+    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42,stratify=y)
+    return X_train,X_test,y_train,y_test
+    
 
-        
+#  签数据集进行标准化
+def standardize(X_train:pd.DataFrame,X_test:pd.DataFrame):
+    scaler=StandardScaler()
+    X_train=scaler.fit_transform(X_train)
+    X_test=scaler.transform(X_test)
+    return X_train,X_test,scaler
+
+def train_svm(X_train:pd.DataFrame,y_train:pd.DataFrame):
+    model=SVC(kernel='rbf')
+    model.fit(X_train,y_train)
+    return model
+
+def predict(model,X_test:pd.DataFrame):
+    y_pred=model.predict(X_test)
+    return y_pred
+
+def evaluate_model(y_test:pd.DataFrame,y_pred:pd.DataFrame):
+    accuracy=accuracy_score(y_test,y_pred)
+    matrix=confusion_matrix(y_test,y_pred) #混淆矩阵
+    return accuracy,matrix
+
