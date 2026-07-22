38fddb6788f343d771e364e655b4d5b7395fda78
zangjinkai-11 <zangjinkai6622@gmail.com>
2026-07-22 12:49:26 +0800

    添加只生成feature_df的extract_feature

--- EEG Explorer/data_pipeline.py
+++ EEG Explorer/data_pipeline.py
@@ -1,25 +1,35 @@
-import main
+import reader
 import channel
 import feature
-
-
-def load_data(path: str):
-    return main.reader.read_edf(path)
-
+import main
+import preprocessing
+import analyser
+import visualization
 def process_one_file(file_path):
-    raw_df = load_data(file_path)
+    raw_df = reader.read_edf(file_path)
     if raw_df is None:
         return
     raw_df = channel.normalize_channel_names(raw_df)
     channels=channel.get_available_channels(raw_df)
-    clean_df,preprocess_result=main.preprocess(raw_df,channels)
-    analysis_result = main.get_features(clean_df,channels)
+    clean_df,preprocess_result=preprocessing.preprocess(raw_df,channels)
+    analysis_result = analyser.get_features(clean_df,channels)
     windows=feature.split_windows(clean_df)
     feature_df=feature.create_feature_dataframe(windows,channels)
-    visualization_result = main.get_visualization(preprocess_result, analysis_result,channels)
+    visualization_result = visualization.get_visualization(preprocess_result, analysis_result,channels)
     return (
         feature_df,
         preprocess_result,
         analysis_result,
         visualization_result
     )
+
+def extract_feature(file_path:str):
+    raw_df = reader.read_edf(file_path)
+    if raw_df is None:
+        return
+    raw_df = channel.normalize_channel_names(raw_df)
+    channels=channel.get_available_channels(raw_df)
+    clean_df,_=main.preprocess(raw_df,channels)
+    windows=feature.split_windows(clean_df)
+    feature_df=feature.create_feature_dataframe(windows,channels)
+    return feature_df
