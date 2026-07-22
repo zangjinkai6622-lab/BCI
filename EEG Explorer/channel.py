bd676de3f2a91b7a4e16aa2ca0bacf369233f695
zangjinkai-11 <zangjinkai6622@gmail.com>
2026-07-22 12:48:52 +0800

    。

--- EEG Explorer/channel.py
+++ EEG Explorer/channel.py
@@ -2,30 +2,18 @@ import pandas as pd
 
 
 def normalize_channel_names(df: pd.DataFrame):
-
     new_columns = []
-
     for col in df.columns:
-
         # 去掉结尾的 .
         col = col.rstrip(".")
-
         new_columns.append(col)
-
-
     df.columns = new_columns
-
     return df
 
 def get_available_channels(df):
-
-    exclude=[
-        "time"
-    ]
-
+    exclude=["time"]
     channels=[
         c for c in df.columns
         if c not in exclude
     ]
-
     return channels
