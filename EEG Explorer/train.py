825856d5fc7e772fba7230dc780968336b943228
zangjinkai-11 <zangjinkai6622@gmail.com>
2026-07-25 13:18:41 +0800

    使用部分文件训练模型

--- EEG Explorer/train.py
+++ EEG Explorer/train.py
@@ -5,7 +5,7 @@ import label
 import machine_learning
 
 def train_main():
-    files=glob.glob("EEG Explorer/data/*.edf")
+    files=glob.glob("EEG Explorer/data/*.edf")[:3]
     features_list=[]
     for file in files:
         result=data_pipeline.process_one_file(file)
@@ -15,10 +15,17 @@ def train_main():
         feature_df['label']=label.get_label(file)
         features_list.append(feature_df)
     dataset=pd.concat(features_list,axis=0,ignore_index=True)
-    print(dataset.drop("label",axis=1).shape)
-    model=machine_learning.create_model("svm")
-    machine_learning.train_pipeline(dataset=dataset,model=model, model_name="svm_v1")
+    machine_learning.train_pipeline(dataset=dataset,model_type='svm', model_name="svm_v1")
 
+def train_all(dataset:pd.DataFrame):
+    models=[
+        ("svm_v1","svm"),
+        ("rf_v1","rf"),
+        ("lr_v1","lr")
+    ]
+    for model_name,model_type in models:
+        model=machine_learning.create_model(model_type)
+        machine_learning.train_pipeline(dataset,model,model_name)
 
 if __name__=="__main__":
     train_main()
