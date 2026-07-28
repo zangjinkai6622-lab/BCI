d7ed0889d89e2daf4e7566720dbf0376734d48d7
zangjinkai-11 <zangjinkai6622@gmail.com>
2026-07-28 14:30:41 +0800

    修改硬编码

--- EEG Explorer/prediction.py
+++ EEG Explorer/prediction.py
@@ -1,11 +1,12 @@
 import config
-
-
+import pandas as pd
+import pathlib
 
 def save_prediction_md(file_name:str,model_name:str,prediction:str):
     save_dir = config.PREDICTION_DIR
     save_dir.mkdir(parents=True,exist_ok=True)
-    save_path=save_dir/"prediction.md"
+    file_stem = pathlib.Path(file_name).stem
+    save_path = save_dir / f"{file_stem}.md"
     with open(save_path,"w",encoding="utf8") as f:
         f.write("# Prediction Report\n\n")
         f.write("## File\n")
@@ -14,13 +15,13 @@ def save_prediction_md(file_name:str,model_name:str,prediction:str):
         f.write(f"{model_name}\n\n")
         f.write("## Prediction\n")
         f.write(f"{prediction}\n")
+        f.write("\n")
+        f.write("## Time\n")
+        f.write(f"{pd.Timestamp.now()}\n")
 
 def save_prediction_csv(results:list):
-
     df = pd.DataFrame(results)
-
     save_path = config.PREDICTION_DIR / "prediction.csv"
-
     df.to_csv(
         save_path,
         index=False,
