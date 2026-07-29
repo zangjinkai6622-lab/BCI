f9aa159ac3abd31054f6ba7ff4108843fceb733b
zangjinkai-11 <zangjinkai6622@gmail.com>
2026-07-29 14:52:28 +0800

    全套流程

--- EEG Explorer/main.py
+++ EEG Explorer/main.py
@@ -1,11 +1,22 @@
 import train
 import inference
+import data_pipeline
+import visualization
+import report
+import compare_models
+import prediction
 
+def main():
+    result = data_pipeline.process_file(
+        "EEG Explorer/data/S001R05.edf"
+    )
+    prediction_result = inference.predict_feature(
+        result["feature_df"]
+    )
+    report.generate_report(
+        analysis_result=result["analysis_result"],
+        prediction_result=prediction_result
+    )
 
 if __name__ == '__main__':
-    mode=input("train or predict:")
-    if mode=="train":
-        train.train_main()
-    elif mode=="predict":
-        result=inference.predict_file("EEG Explorer/data/S001R003.edf")
-        print(result)
+    main()
