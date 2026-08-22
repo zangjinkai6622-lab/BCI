6708c8ff7bfe0d67bf5faaf594647e1ca2841501
zangjinkai-11 <zangjinkai6622@gmail.com>
2026-08-22 09:37:07 +0800

    accuracy计算

--- EEG Explorer/deep_learning/jihuohanshu.py
+++ EEG Explorer/deep_learning/jihuohanshu.py
@@ -39,6 +39,9 @@ loss_fn=nn.MSELoss()
 optimizer=torch.optim.SGD(model.parameters(),lr=0.01)
 
 for epoth in range(1000):    
+    # 有些网络层在训练和测试时是不一样的，比如DropOut、BatchNorm，所以需要显示告诉是在训练还是测试模式。
+    # 训练模式
+    model.train()
     # pytorth会保留上次的梯度，清空上一次留下的Gradient。
     optimizer.zero_grad()
     prediction=model(x)
@@ -49,3 +52,26 @@ for epoth in range(1000):
     optimizer.step()
     if epoth%10==0:
         print(epoth,loss.item())
+
+    # model.eval() # 测试模式 
+    # # 测试模式下，不需要计算梯度
+    # with torch.no_grad():
+    #     for x, y in val_loader:
+    #         prediction = model(x)
+
+
+prediction = torch.tensor([
+    [2.1, 0.3, 0.5],
+    [0.2, 3.5, 0.1],
+    [0.1, 0.4, 2.8],
+    [1.2, 2.3, 0.5]
+])
+
+y = torch.tensor([0, 1, 2, 1])
+
+prediction_result=prediction.argmax(dim=1) #在每一行中，返回每行最大值的索引。
+correct=(prediction_result==y)
+accuracy=correct.float().mean() # .float()将tensor转换成float类型（true->1,false->0），.mean()求平均值。
+print(prediction_result)
+print(correct)
+print(accuracy)
