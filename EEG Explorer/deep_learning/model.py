2d4644d3017f1db39c5768c74871ea4943ddb54a
zangjinkai-11 <zangjinkai6622@gmail.com>
2026-09-03 08:51:09 +0800

    。

--- EEG Explorer/deep_learning/model.py
+++ EEG Explorer/deep_learning/model.py
@@ -5,11 +5,11 @@ model = nn.Sequential(
     nn.Linear(592, 128),
     nn.BatchNorm1d(128),
     nn.ReLU(),
-    nn.Dropout(0.3),
+    nn.Dropout(0.5),
     nn.Linear(128, 64),
     nn.BatchNorm1d(64),
     nn.ReLU(),
-    nn.Dropout(0.3),
+    nn.Dropout(0.5),
     nn.Linear(64, 3),
 )
 
